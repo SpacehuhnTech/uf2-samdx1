f4213db24a258231dd7590c04730b4d0530bba1e
Spacehuhn <github@spacehuhn.de>
2021-01-25 10:21:55 +0100

    Malduino

---
+++ boards/malduino/board_config.h
@@ -0,0 +1,22 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "Maltronics"
+#define PRODUCT_NAME "Mal2.0"
+#define VOLUME_LABEL "Mal"
+#define INDEX_URL "http://malboot.maltronics.com"
+#define BOARD_ID "MalW2.0"
+
+#define USB_VID 0x16D0
+#define USB_PID 0x103F
+
+#define LED_PIN PIN_PA10
+// #define LED_TX_PIN PIN_PA27
+// #define LED_RX_PIN PIN_PB03
+
+#define BOARD_RGBLED_CLOCK_PIN            PIN_PA01
+#define BOARD_RGBLED_DATA_PIN             PIN_PA00
+
+#endif // ifndef BOARD_CONFIG_H
